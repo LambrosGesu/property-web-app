5f92b646e2cc745a8a7aeae8302ada923b657f9c
Doug <barkaniza@hotmail.com>
2020-12-14 20:54:59 +0200

    Edit Owners View

--- src/main/java/gr/codehub/team7/propertywebapp/controller/admin/OwnerController.java
+++ src/main/java/gr/codehub/team7/propertywebapp/controller/admin/OwnerController.java
@@ -9,7 +9,12 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Optional;
 
 @Controller
 public class OwnerController {
@@ -35,4 +40,22 @@ public class OwnerController {
         ownerService.insertOwner(owner);
         return "redirect:/owners";
     }
+
+    @GetMapping("{id}/edit-owner")
+    public String editOwner(Model model, @PathVariable Long id){
+
+           Optional<Owner> owner= ownerService.findOwnerById(id);
+           model.addAttribute("owner",owner.get());
+           model.addAttribute(PROPERTY_TYPE,PropertyType.values());
+           return "editowner";
+    }
+
+    @PostMapping("{id}/edit-owner")
+    public  String editOwner(@ModelAttribute Owner owner, @PathVariable Long id){
+        //Optional<Owner> ownerId=ownerService.findOwnerBySsn(owner.getSsn());
+        ownerService.updateOwner(owner,id);
+        return  "redirect:/owners";
+    }
+
+
 }
