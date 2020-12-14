72b9fade45213f97fb1b8626e47844c5234891a1
Vasilis Nikoulis <vasilisnikoulis@gmail.com>
2020-12-14 21:26:52 +0200

    x button

--- src/main/java/gr/codehub/team7/propertywebapp/controller/admin/RepairController.java
+++ src/main/java/gr/codehub/team7/propertywebapp/controller/admin/RepairController.java
@@ -3,8 +3,6 @@ package gr.codehub.team7.propertywebapp.controller.admin;
 import gr.codehub.team7.propertywebapp.domain.Repair;
 import gr.codehub.team7.propertywebapp.enums.JobType;
 import gr.codehub.team7.propertywebapp.enums.Status;
-import gr.codehub.team7.propertywebapp.forms.EditRepairForm;
-import gr.codehub.team7.propertywebapp.forms.RepairSearchForm;
 import gr.codehub.team7.propertywebapp.service.OwnerService;
 import gr.codehub.team7.propertywebapp.service.RepairService;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -15,11 +13,9 @@ import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 
-import java.text.DecimalFormat;
-
 @Controller
 public class RepairController {
-    private static final String REPAIRS_LIST = "repairs";
+
     @Autowired
     private RepairService repairService;
 
@@ -40,36 +36,15 @@ public class RepairController {
         return "createrepair";
     }
 
-    @GetMapping("/repairs/search")
-    public String searchRepairs(Model model) {
-        return "pages/searchRepairs";
-    }
-
-    @PostMapping("/repairs/search")
-    public String returnRepairs(@ModelAttribute("repairSearchForm") RepairSearchForm repairSearchForm, Model model) {
-        model.addAttribute(REPAIRS_LIST,repairService.findBySearchForm(repairSearchForm));
-        return "pages/searchRepairs";
-    }
-
     @PostMapping("/repair/create")
     public String createRepairPost(Model model, @ModelAttribute Repair repair){
         repairService.insertRepair(repair);
         return "redirect:/repairs";
     }
 
-    @GetMapping("{id}/editrepair")
-    public String editRepair(Model model, @PathVariable Long id){
-        model.addAttribute("status",Status.values());
-        model.addAttribute("jobTypes",JobType.values());
-        model.addAttribute("repair",repairService.findById(id).get());
-        model.addAttribute("owners",ownerService.getAllOwners());
-        return "editrepair";
-    }
-
-    @PostMapping("{id}/editrepair")
-    public String editRepair(@ModelAttribute EditRepairForm repair, @PathVariable Long id){
-
-        repairService.updateRepair(repair,id);
+    @PostMapping("/repair/{id}/delete")
+    public  String deleteRepair(@PathVariable Long id){
+        repairService.deleteRepairById(id);
         return "redirect:/repairs";
     }
 }
---
+++ src/main/resources/static/js/main.js
@@ -0,0 +1,9 @@
+// jQuery(function ($){
+//     $('#remove').on('show.bs.modal', function (event) {
+//         const id = event.relatedTarget.dataset.id;
+//         $('#deleteForm').attr('action', `/repair/${id}/delete`);
+//         $('.modal-title').text(function () {
+//             return `Repair #${id}`;
+//         });
+//     });
+//     });
